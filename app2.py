69e46827b7d7bb59d25c965f1e000018ce951107
deepanshudey <deepanshudey123@gmail.com>
2021-11-16 22:20:10 -0500

    updates

--- app2.py
+++ app2.py
@@ -1,5 +1,5 @@
 # app2.py
 import streamlit as st
-def app():
+def app(conn):
     st.title('APP2')
     st.write('Welcome to app2')
--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,7 @@ import app2
 def init_connection():
     return psycopg2.connect(**st.secrets["postgres"])
 
+conn = init_connection()
 PAGES = {
     "App1": app1,
     "App2": app2
@@ -17,4 +18,4 @@ PAGES = {
 st.sidebar.title('Navigation')
 selection = st.sidebar.radio("Go to", list(PAGES.keys()))
 page = PAGES[selection]
-page.app()
+page.app(conn)
